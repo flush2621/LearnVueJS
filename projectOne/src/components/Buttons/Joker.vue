7191c48c3192b03ed960c76be0ae3b77a7bfab00
liupeng <6651815@qq.com>
2019-10-22 12:00:14 +0800

    Add Buttons

---
+++ projectOne/src/components/Buttons/Joker.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="joker">55</div>
+</template>
+<style>
+.joker{
+    position: relative;
+    margin: 5px 0;
+    border-radius: 4px;
+    width: 40px;
+    height: 30px;
+    top: -10px;
+    background-color: red;
+    overflow: hidden;
+}
+.joker::before{
+    content: 'xx';
+}
+.joker::after{
+    content: "yy";
+}
+</style>
--- projectOne/src/components/setting/ButtonsCollect.vue
+++ projectOne/src/components/setting/ButtonsCollect.vue
@@ -7,6 +7,12 @@
         <div><play-button /></div>
         <div><floating-button /></div>
         <div><progress-bar /></div>
+        <div><badge /></div>
+        <div><land-mark /></div>
+        <div><heart /></div>
+        <div><mail /></div>
+        <div><calendar /></div>
+        <div><joker /></div>
     </div>
 </template>
 <script>
@@ -17,6 +23,12 @@ import EnergyBall from '../Buttons/EnergyBall'
 import PlayButton from '../Buttons/PlayButton'
 import FloatingButton from '../Buttons/FloatingButton'
 import ProgressBar from '../Buttons/ProgressBar'
+import Badge from '../Buttons/Badge'
+import LandMark from '../Buttons/LandMark'
+import Heart from '../Buttons/Heart'
+import Mail from '../Buttons/Mail'
+import Calendar from '../Buttons/Calendar'
+import Joker from '../Buttons/Joker'
 export default {
     components:{
         CircleLoading,
@@ -25,16 +37,23 @@ export default {
         EnergyBall,
         PlayButton,
         FloatingButton,
-        ProgressBar
+        ProgressBar,
+        Badge,
+        LandMark,
+        Heart,
+        Mail,
+        Calendar,
+        Joker
     }
 }
 </script>
 <style>
 .home{
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(4, 1fr);
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(5, 1fr);
     height: 80vh;
+    align-items: center;
     justify-items: center;
     /* 修改位置 */
     position: absolute;
